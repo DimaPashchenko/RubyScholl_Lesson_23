2b8bc57a26e253065b2b3400b753560b5f4abe4f
Дмитрий Пащенко <kitaminchik@gmail.com>
2022-01-09 16:27:08 +0200

    code for Lessons 23

---
+++ app.rb
@@ -0,0 +1,50 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/about' do
+	erb :about
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+ @user_name=params[:username]
+ @phone=params[:phone]
+ @date_time=params[:datetime]
+ @barber=params[:barber]
+
+ @title = 'Thank you!'
+ @message= "Dear #{@user_name}, we'll be waiting for you at #{@date_time} "
+
+ f=File.open 'users.txt', 'a'
+ f.write "User: #{@user_name}, Phone: #{@phone}, Date and time: #{@date_time}, Barber: #{@barber}\n"
+ f.close
+ erb :message
+end
+
+get '/contacts' do
+	erb :contacts
+end
+
+get '/admin' do
+	erb :admin	
+end
+
+post '/admin' do
+	@login=params[:aaa]
+	@password=params[:bbb]
+
+	if @login == 'admin' && @password == 'secret'
+	@logfile = File.read ("users.txt")
+	erb :list
+	else
+	@messageaccess = 'Access denied'
+    end
+end
